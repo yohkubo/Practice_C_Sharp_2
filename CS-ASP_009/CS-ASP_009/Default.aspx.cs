048d7673366c9773eacd756e3558764f75fcbb43
Yohei <yo_kubocchi@hotmail.com>
2018-03-07 00:32:51 -0800

    Added

---
+++ CS-ASP_009/CS-ASP_009/Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_009
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            int j = 2;
+            //int result = i + j;
+            //int result = i - j;
+            //int result = i * j;
+            //int result = i / j;
+
+            /*
+            i = i + 5;
+            i += 5;
+            i++;
+            i --;
+            */
+
+            //int myInteger = (5 + 1) * 7;
+            //resultLabel.Text = myInteger.ToString();
+
+            //double myDouble = 5.5;
+            //int myInteger = 7;
+            //int myOtherInteger = 4;
+
+            //double myResult = myDouble + myInteger;
+            //int myResult = (int)myDouble + myInteger;
+            //int myResult = myInteger / myOtherInteger;
+            //double myResult = (double)myInteger / (double)myOtherInteger;
+
+            //resultLabel.Text = myResult.ToString();
+
+            int firstNumber = 2000000000;
+            int secondNumber = 2000000000;
+            long resultNumber = firstNumber * secondNumber;
+
+            checked
+            {
+                resultNumber = firstNumber + secondNumber;
+            }
+
+            resultLabel.Text = resultNumber.ToString();
+
+
+
+        }
+    }
+}
---
+++ CS-ASP_009/CS-ASP_009/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CS_ASP_009.Global" Language="C#" %>
