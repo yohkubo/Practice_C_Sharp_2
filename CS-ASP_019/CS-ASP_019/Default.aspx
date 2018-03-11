2dff7d4ef0955ceea3f64d55b23ac71e4b69f832
Yohei <yo_kubocchi@hotmail.com>
2018-03-11 15:31:24 -0700

    Practices

---
+++ CS-ASP_019/CS-ASP_019/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_019.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Loan Application Form<br />
+            <br />
+            Name:&nbsp;
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Phone Number:&nbsp;
+            <asp:TextBox ID="phoneTextBox" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            Social Security Number:&nbsp;
+            <asp:TextBox ID="ssnTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Loan Origination Date:<br />
+            <asp:Calendar ID="myCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Salary:&nbsp;
+            <asp:TextBox ID="salaryTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Submit" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_019/CS-ASP_019/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_019
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            //string result = String.Format("Thank your, {0}, for your business", nameTextBox.Text);
+
+            int ssn = int.Parse(ssnTextBox.Text);
+            //string result = String.Format("Thank you, {0}, for your business. <br />Your Social Security Number is: {1:000-00-0000}", nameTextBox.Text, ssn);
+
+            int phone = int.Parse(phoneTextBox.Text);
+            //string result = String.Format("Thank you, {0}, for your business.<br /> " +
+            //                            "Your Social Security Number is: {1:000-00-0000}<br />" +
+            //                            "Phone number: {2:000(000)0000}", nameTextBox.Text, ssn, phone);
+
+            /*
+            string result = String.Format("Thank you, {0}, for your business." +
+                "<br />Your Social Security Number: {1:111-11-1111}" +
+                "<br />Phone Number: {2:000(000)0000}" +
+                "<br />Loan Date: {3:MMM-dd(ddd), yyyy}", 
+                nameTextBox.Text,
+                ssn,
+                phone,
+                myCalendar.SelectedDate);
+            */
+
+
+            resultLabel.Text = result;
+
+        }
+    }
+}
